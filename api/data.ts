8602abe8bde840ffeaf997f360477c4e9727442b
Jorgen Nystad <jorgen@nystad.cc>
2020-03-22 12:51:49 +0100

    Serverless, set cache control

--- api/data.ts
+++ api/data.ts
@@ -4,6 +4,10 @@ import request from "superagent";
 module.exports = (req: NowRequest, res: NowResponse) => {
   request
     .get("https://data.norge.no/api/dcat/data.json?page=" + req.query.page)
-    .then(r => res.json(r.body))
+    .then(r => {
+      res.removeHeader("Cache-Control");
+      res.setHeader("Cache-Control", "public, max-age=3600");
+      res.json(r.body);
+    })
     .catch(() => res.status(500));
 };
